49c002ac5c67bf1ebd34358250d8ab39ddde1261
EthanShklar <eitan.shklar@gmail.com>
2026-01-07 15:39:05 +0200

    Add upsideDown method to reverse a stack
    Implemented upsideDown method to reverse a stack using a queue.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Stack<Integer> S1 = new Stack<>();
+	    S1.push(5);
+	    S1.push(4);
+	    S1.push(91);
+	    System.out.println(S1);
+	    upsideDown(S1);
+	    System.out.println(S1);
+		
+	}
+	
+	public static void upsideDown(Stack<Integer> stack)
+	{
+	    Queue<Integer> q = new Queue<>();
+	    while (!stack.isEmpty())
+	    {
+	        q.insert(stack.pop());
+	    }
+	    while(!q.isEmpty())
+	    {
+	        stack.push(q.remove());
+	    }
+	}
 }
