cef62002b9e094d84fa666a67ffa3bebd09b8c1f
EthanShklar <eitan.shklar@gmail.com>
2026-01-07 15:44:02 +0200

    Introduce Queue and upsideDownQueue method
    Added a Queue and implemented upsideDownQueue method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,19 @@ public class Main
 	public static void main(String[] args) 
 	{
 	    Stack<Integer> S1 = new Stack<>();
+	    Queue<Integer> Q1 = new Queue<>();
 	    S1.push(5);
 	    S1.push(4);
 	    S1.push(91);
+	    Q1.insert(7);
+	    Q1.insert(6);
+	    Q1.insert(30);
 	    System.out.println(S1);
 	    upsideDown(S1);
 	    System.out.println(S1);
+	    System.out.println(Q1);
+	    upsideDownQueue(Q1);
+	    System.out.println(Q1);
 		
 	}
 	
@@ -22,6 +29,19 @@ public class Main
 	    while(!q.isEmpty())
 	    {
 	        stack.push(q.remove());
+	    }
+	}
+	
+	public static void upsideDownQueue(Queue<Integer> q)
+	{
+	    Stack<Integer> stack = new Stack<>();
+	    while(!q.isEmpty())
+	    {
+	        stack.push(q.remove());
+	    }
+	    while(!stack.isEmpty())
+	    {
+	        q.insert(stack.pop());
 	    }
 	}
 }
